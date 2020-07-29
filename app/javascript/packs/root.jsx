04a362904191e782cf4001e966f9a4378e042c3e
cathyli229 <hli50@u.rochester.edu>
2020-07-29 13:24:47 -0700

    Local Storage

--- app/javascript/packs/root.jsx
+++ app/javascript/packs/root.jsx
@@ -8,18 +8,29 @@ import Main from './Main';
 import '../../../app/javascript/helpers/initFA';
 import configureStore from '../store/store';
 import { fetchAllTicks } from '../actions/tick_actions';
+import { loadState, saveState } from '../store/localStorage';
+import throttle from 'lodash/throttle';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Main>
       <App />
+
     </Main>
   </Provider>
 );
 
 document.addEventListener('DOMContentLoaded', () => {
-  const preloadedState = { ...localStorage };
+  // const preloadedState = { ...localStorage };
+  const preloadedState = loadState();
   const store = configureStore(preloadedState);
+  store.subscribe(
+  throttle(() => {
+    saveState({
+      transactions: store.getState().transactions,
+    });
+  }, 1000)
+  );
   window.fetchAllTicks = fetchAllTicks;
   window.store = store;
   ReactDOM.render(<Root store={store} />, document.body.appendChild(document.createElement('div')));
