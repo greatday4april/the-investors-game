c9bef52cab93843a67dad20c848a20dc1bf2f9ec
xiaojun.wu <wuxiaojun2018gt@outlook.com>
2020-08-24 23:38:36 -0600

    add style to PaymentLineChart and Button

--- app/javascript/components/dashboard/StatsTable.js
+++ app/javascript/components/dashboard/StatsTable.js
@@ -7,7 +7,7 @@ const StatsTable = (props) => {
   return (
     <div
       className="align-items-center border-bottom border-grey border-2x py-3"
-      style={{ justifyContent: 'space-between', fontSize: '1.25rem' }}
+      style={{ justifyContent: 'space-between', fontSize: '2.45rem' }}
     >
       <Col xs="auto" className="pl-3 mx-0 text-sub-title">
         {keyWord}
--- app/javascript/components/dashboard/TicksChart.js
+++ app/javascript/components/dashboard/TicksChart.js
@@ -21,21 +21,7 @@ const TicksChart = (props) => {
   } else {
     let stats = props.stats[symbol];
     statsComponent = (
-      <div className="shadowed-card"
-        // style={{
-        //   width: '811px',
-        //   height: '584px',
-        //   display: 'block',
-        //   border: 'solid 1px white',
-        //   backgroundColor: 'white',
-        //   borderRadius: '20px',
-        //   padding: '10px',
-        //   background: '#F9F4E9',
-        //   boxShadow: '6px 6px 0px #000000',
-        //   boxSizing: 'border-box',
-        //   border: '4px solid #000000',
-        // }}
-      >
+      <div className="shadowed-card">
         <div
           className="text-title"
           style={{ width: '58px', height: '24px', left: '529px', top: '713px', marginLeft: '30px', marginTop: '30px' }}
@@ -70,53 +56,51 @@ const TicksChart = (props) => {
 
   return (
     <div>
-      <PaymentsLineChartContainer period={scale} symbol={symbol} />
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Button
-          color="link"
-          size="sm"
-          className="ml-4 mb-2"
-          style={{ fontSize: '1.44rem' }}
-          onClick={() => setScale(ONE_DAY_DURATION)}
-        >
-          1 DAY
-        </Button>
-        <Button
-          color="link"
-          size="sm"
-          className="ml-4 mb-2"
-          style={{ fontSize: '1.44rem' }}
-          onClick={() => setScale(ONE_WEEK_DURATION)}
-        >
-          1 WEEK
-        </Button>
-        <Button
-          color="link"
-          size="sm"
-          className="ml-4 mb-2"
-          style={{ fontSize: '1.44rem' }}
-          onClick={() => setScale(ONE_MONTH_DURATION)}
-        >
-          1 MONTH
-        </Button>
-        <Button
-          color="link"
-          size="sm"
-          className="ml-4 mb-2"
-          style={{ fontSize: '1.44rem' }}
-          onClick={() => setScale(SIX_MONTHS_DURATION)}
-        >
-          6 MONTHS
-        </Button>
-        <Button
-          color="link"
-          size="sm"
-          className="ml-4 mb-2"
-          style={{ fontSize: '1.44rem' }}
-          onClick={() => setScale(ONE_YEAR_DURATION)}
-        >
-          1 YEAR
-        </Button>
+      <div className="shadowed-card mb-2r" >
+        <PaymentsLineChartContainer period={scale} symbol={symbol} />
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+            className="time-scale">
+          <Button
+            color="link"
+            size="sm"
+            className="p-lr"
+            onClick={() => setScale(ONE_DAY_DURATION)}
+          >
+            1 DAY
+          </Button>
+          <Button
+            color="link"
+            size="sm"
+            className="p-lr"
+            onClick={() => setScale(ONE_WEEK_DURATION)}
+          >
+            1 WEEK
+          </Button>
+          <Button
+            color="link"
+            size="sm"
+            className="p-lr"
+            onClick={() => setScale(ONE_MONTH_DURATION)}
+          >
+            1 MONTH
+          </Button>
+          <Button
+            color="link"
+            size="sm"
+            className="p-lr"
+            onClick={() => setScale(SIX_MONTHS_DURATION)}
+          >
+            6 MONTHS
+          </Button>
+          <Button
+            color="link"
+            size="sm"
+            className="p-lr"
+            onClick={() => setScale(ONE_YEAR_DURATION)}
+          >
+            1 YEAR
+          </Button>
+        </div>
       </div>
       {statsComponent}
     </div>
